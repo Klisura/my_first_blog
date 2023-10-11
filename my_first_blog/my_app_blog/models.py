b8ffb89ca44e8563bb77e3d8ddfec399ea1eecba
Milan Trajkovic <lakocemo@outlook.com>
2023-10-10 21:43:20 -0400

    blog_post

--- my_first_blog/my_app_blog/models.py
+++ my_first_blog/my_app_blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+STATUS = ((0, "Draft"),(1, "Published"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, null=False, blank="False")
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
+
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
+
+class Meta:
+    ordering = ['- created']
